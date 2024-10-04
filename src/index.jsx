7399b39341978cab3e819812b273b2ec8cc751f7
AjhayRam <84840076+AjhayRam@users.noreply.github.com>
2024-10-04 23:00:12 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,14 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import appRouter from "./routes/routes";
+import Home from "./pages/Home.jsx";
 import {BrowserRouter, Routes, Route} from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 <React.StrictMode>
     <BrowserRouter basename = {import.meta.env.BASE_URL}>
         <Routes>
-            < Route path="*" element={<App />} />
+            < Route path="*" element={<Home />} />
         </Routes>
     </BrowserRouter>
 </React.StrictMode>
